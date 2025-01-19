5d19fc9ac51d4aa436481ad13c12520580ba8e54
Roy_Naor <133534625+RoyNaor@users.noreply.github.com>
2025-01-19 17:55:31 +0200

    Update traceroute.h

--- Ex4/Traceroute/traceroute.h
+++ Ex4/Traceroute/traceroute.h
@@ -1,6 +1,10 @@
 #ifndef TRACEROUTE_H
 #define TRACEROUTE_H
 
+#include <netinet/ip.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+
 // Constants
 #define TIMEOUT 1000          // Timeout for responses (in ms)
 #define MAX_HOPS 30           // Maximum TTL value
@@ -9,5 +13,7 @@
 
 // Function prototypes
 unsigned short int calculate_checksum(void *data, unsigned int bytes);
+double calculate_rtt(struct timeval *start, struct timeval *end);
+void build_ip_header(struct iphdr *ip_hdr, struct sockaddr_in *dest_addr, int ttl, int payload_len);
 
 #endif // TRACEROUTE_H
